a25bde23f626f79c0ac13183d62b16bda588db2b
Rock Hymas <rhymas@trackabout.com>
2012-11-14 05:35:39 +0700

    Split spec and generic spec into separate files

---
+++ TrackAbout.Mobile.ContextSpecifications/GenericSpec.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NUnit.Framework;
+using Ninject;
+using Ninject.Activation.Providers;
+using Ninject.MockingKernel;
+using Ninject.MockingKernel.RhinoMock;
+using Ninject.Planning.Bindings;
+using Rhino.Mocks;
+using Rhino.Mocks.Interfaces;
+
+namespace SpecEasy
+{
+    [TestFixture]
+    public class Spec<TUnit> : Spec
+    {
+        protected MockingKernel MockingKernel;
+
+        protected T Mock<T>() where T : class
+        {
+            return MockRepository.GenerateMock<T>();
+        }
+
+        protected T Get<T>()
+        {
+            return MockingKernel.Get<T>();
+        }
+
+        protected void Set<T>(T item)
+        {
+            var binding = new Binding(typeof(T))
+            {
+                ProviderCallback = ctx => new ConstantProvider<T>(item)
+            };
+            MockingKernel.AddBinding(binding);
+        }
+
+        protected void Raise<T>(Action<T> eventSubscription, params object[] args) where T : class
+        {
+            var mock = Get<T>();
+            mock.Raise(eventSubscription, args);
+        }
+
+        protected void AssertWasCalled<T>(Action<T> action)
+        {
+            var mock = Get<T>();
+            mock.AssertWasCalled(action);
+        }
+
+        protected void AssertWasCalled<T>(Action<T> action, Action<IMethodOptions<object>> methodOptions)
+        {
+            var mock = Get<T>();
+            mock.AssertWasCalled(action, methodOptions);
+        }
+
+        protected void AssertWasNotCalled<T>(Action<T> action)
+        {
+            var mock = Get<T>();
+            mock.AssertWasNotCalled(action);
+        }
+
+        protected void AssertWasNotCalled<T>(Action<T> action, Action<IMethodOptions<object>> methodOptions)
+        {
+            var mock = Get<T>();
+            mock.AssertWasNotCalled(action, methodOptions);
+        }
+
+        protected override void InitializeTest()
+        {
+            base.InitializeTest();
+            MockingKernel = new RhinoMocksMockingKernel();
+        }
+
+        protected TUnit SUT
+        {
+            get { return Get<TUnit>(); }
+            set { Set(value); }
+        }
+    }
+}
--- TrackAbout.Mobile.ContextSpecifications/Spec.cs
+++ TrackAbout.Mobile.ContextSpecifications/Spec.cs
@@ -1,17 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using NUnit.Framework;
-using Ninject;
-using Ninject.Activation.Providers;
-using Ninject.MockingKernel;
-using Ninject.MockingKernel.RhinoMock;
-using Ninject.Planning.Bindings;
-using Rhino.Mocks;
-using Rhino.Mocks.Interfaces;
 
 namespace SpecEasy
 {
@@ -181,73 +174,6 @@ namespace SpecEasy
         private static string Indent(string text, int depth)
         {
             return new string(' ', depth * 2) + text;
-        }
-    }
-
-    [TestFixture]
-    public class Spec<TUnit> : Spec
-    {
-        protected MockingKernel MockingKernel;
-
-        protected T Mock<T>() where T : class
-        {
-            return MockRepository.GenerateMock<T>();
-        }
-
-        protected T Get<T>()
-        {
-            return MockingKernel.Get<T>();
-        }
-
-        protected void Set<T>(T item)
-        {
-            var binding = new Binding(typeof(T))
-            {
-                ProviderCallback = ctx => new ConstantProvider<T>(item)
-            };
-            MockingKernel.AddBinding(binding);
-        }
-
-        protected void Raise<T>(Action<T> eventSubscription, params object[] args) where T : class
-        {
-            var mock = Get<T>();
-            mock.Raise(eventSubscription, args);
-        }
-
-        protected void AssertWasCalled<T>(Action<T> action)
-        {
-            var mock = Get<T>();
-            mock.AssertWasCalled(action);
-        }
-
-        protected void AssertWasCalled<T>(Action<T> action, Action<IMethodOptions<object>> methodOptions)
-        {
-            var mock = Get<T>();
-            mock.AssertWasCalled(action, methodOptions);
-        }
-
-        protected void AssertWasNotCalled<T>(Action<T> action)
-        {
-            var mock = Get<T>();
-            mock.AssertWasNotCalled(action);
-        }
-
-        protected void AssertWasNotCalled<T>(Action<T> action, Action<IMethodOptions<object>> methodOptions)
-        {
-            var mock = Get<T>();
-            mock.AssertWasNotCalled(action, methodOptions);
-        }
-
-        protected override void InitializeTest()
-        {
-            base.InitializeTest();
-            MockingKernel = new RhinoMocksMockingKernel();
-        }
-
-        protected TUnit SUT
-        {
-            get { return Get<TUnit>(); }
-            set { Set(value); }
         }
     }
 }
