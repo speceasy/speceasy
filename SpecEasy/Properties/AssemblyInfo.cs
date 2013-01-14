976f68f5fa44772f1c1963954bb5eda6e896b813
Rock Hymas <rhymas@trackabout.com>
2013-01-15 04:19:55 +0700

    Update library properties

--- SpecEasy/Properties/AssemblyInfo.cs
+++ SpecEasy/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TrackAbout, Inc.")]
 [assembly: AssemblyProduct("SpecEasy")]
-[assembly: AssemblyCopyright("Copyright ©2010 TrackAbout, Inc.")]
+[assembly: AssemblyCopyright("Copyright ©2013 TrackAbout, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 
