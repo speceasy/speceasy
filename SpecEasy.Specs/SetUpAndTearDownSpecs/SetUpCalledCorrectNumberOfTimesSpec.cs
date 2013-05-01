9af68a0cf2df8bfdb09730ae53d2d5a866baeab1
Joel Ross <jross@rosscode.com>
2013-05-01 14:47:28 -0400

    Added test project with tests for SetUp and TearDown methods. Added FizzBuzz to be able to demo tests in the readme file

---
+++ SpecEasy.Specs/SetUpAndTearDownSpecs/SetUpCalledCorrectNumberOfTimesSpec.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace SpecEasy.Specs.SetUpAndTearDownSpecs
+{
+    public class SetUpCalledCorrectNumberOfTimesSpec : Spec<FakeClass>
+    {
+        private int timesCalled = 0;
+        public void CountSetUpCalls()
+        {
+            When("running a test that overrides SetUp", () => SUT.DoNothing());
+            Then("it should only call the overriden method once to start with", () => Assert.That(timesCalled, Is.EqualTo(1)));
+            Then("it should call the overriden method a second time for the next test", () => Assert.That(timesCalled, Is.EqualTo(2)));
+             
+        }
+
+        protected override void SetUp()
+        {
+            base.SetUp();
+            timesCalled++;
+        }
+         
+    }
+}
---
+++ SpecEasy.Specs/SetUpAndTearDownSpecs/TearDownGetsCalledSpec.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace SpecEasy.Specs.SetUpAndTearDownSpecs
+{
+    public class TearDownGetsCalledSpec : Spec<FakeClass>
+    {
+        private int timesCalled = 0;
+
+        public void TearDownSpec()
+        {
+            timesCalled = 0;
+
+            When("running a test in a class that overrides FinalizeTest", () => SUT.DoNothing());
+            Then("it should not call finalize test before the first Then", () => Assert.That(timesCalled, Is.EqualTo(0)));
+            Then("it should call finalize test before the second Then", () => Assert.That(timesCalled, Is.EqualTo(1)));
+        }
+
+        public void TearDownWithGivenSpec()
+        {
+            timesCalled = 0;
+
+            When("running a test in a class that overrides FinalizeTest", () => SUT.DoNothing());
+
+            Given("there is a given", () => SUT.DoNothing()).Verify(() => {
+                Then("it should not call finalize test before the first Then", () => Assert.That(timesCalled, Is.EqualTo(0)));
+                Then("it should call finalize test before the second Then", () => Assert.That(timesCalled, Is.EqualTo(1)));
+            });
+
+            Given("there is another given", () => SUT.DoNothing()).Verify(() =>
+                {
+                    Then("it should call finalize test again before the third Then", () => Assert.That(timesCalled, Is.EqualTo(2)));
+                    Given("we have nested givens", () => SUT.DoNothing()).Verify(() => 
+                        Then("it should call finalize test again before the nested Then", () => Assert.That(timesCalled, Is.EqualTo(3))));
+                });
+        }
+
+        protected override void TearDown()
+        {
+            base.TearDown();
+            timesCalled++;
+        }
+    }
+}
