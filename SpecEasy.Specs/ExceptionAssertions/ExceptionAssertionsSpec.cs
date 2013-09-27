d1f64c1fb966769f7945d3412a6a5962f5b6227c
Ron Ratzlaff <ron@ratzlaff.us>
2013-09-26 19:40:38 -0600

    Issue #12: move spec examples to own class to separate concepts.

---
+++ SpecEasy.Specs/ExceptionAssertions/ExceptionAssertionsSpec.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SpecEasy.Specs.ExceptionAssertions
+{
+    public class ExceptionAssertionsSpec : Spec<SimpleCalculator>
+    {
+        public void Run()
+        {
+            var result = 0;
+            var number1 = 0;
+            var number2 = 0;
+
+            When("using the simple calculator", () => result = SUT.Add(number1, number2));
+
+            Given("the first number is 1", () => number1 = 1).Verify(() => {
+                Given("the second number is 2", () => number2 = 2).Verify(() => 
+                    Then("the result should be 3", () => Assert.That(result, Is.EqualTo(3))));
+
+                Given("the second number is 11", () => number2 = 11).Verify(() => {
+                    Then("it should throw an argument out of range exception", () => AssertWasThrown<ArgumentOutOfRangeException>());
+                    Then("the thrown exception should indicate what argument is in error", 
+                        () => AssertWasThrown<ArgumentOutOfRangeException>(ex => Assert.That(ex.ParamName, Is.EqualTo("number2"))));});});
+        }    
+    }
+
+    public class SimpleCalculator
+    {
+        public int Add(int number1, int number2)
+        {
+            if (number2 > 10)
+                throw new ArgumentOutOfRangeException("number2");
+
+            return number1 + number2;
+        }
+    }
+}
--- SpecEasy.Specs/FizzBuzz/FizzBuzzSpecs.cs
+++ SpecEasy.Specs/FizzBuzz/FizzBuzzSpecs.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace SpecEasy.Specs.FizzBuzz
 {
@@ -32,13 +31,6 @@ namespace SpecEasy.Specs.FizzBuzz
 
             Given("an input of a multiple of 3 and 5", () => input = 30).Verify(() =>
                 Then("it should return fizzbuzz", () => Assert.That(result, Is.EqualTo("fizzbuzz"))));
-
-            Given("an input of 7", () => input = 7).Verify(() =>
-                Then("it should throw an argument exception", () => AssertWasThrown<ArgumentException>()));
-
-            Given("an input of a multiple 7", () => input = 7).Verify(() =>
-                Then("it should throw an argument exception for the expected argument", 
-                    () => AssertWasThrown<ArgumentException>(ex => Assert.That(ex.ParamName, Is.EqualTo("input")))));
         }
     }
 
@@ -56,11 +48,6 @@ namespace SpecEasy.Specs.FizzBuzz
             if (input%5 == 0)
             {
                 retVal += "buzz";
-            }
-
-            if (input%7 == 0)
-            {
-                throw new ArgumentException("The input must not be a multiple of 7.", "input");
             }
 
             return retVal ?? input.ToString();
