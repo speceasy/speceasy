96a1aa1bec8b1be8b79bc1ce73b1f97953313808
bpruitt-goddard <bpruitt-goddard@github>
2013-10-06 17:42:16 -0400

    Moved mock spec into IMockingFramework template
    Added IMockingFramework template
    Added concrete RhinoMock mocking impl into MockAdapters.RhinoMocks
    SpecEasy proj now has dep on RhinoMock project instead of RhinoMock directly

---
+++ SpecEasy.MockAdapters.RhinoMocks/IRhinoMockingFramework.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Rhino.Mocks.Interfaces;
+using SpecEasy.Core;
+
+namespace SpecEasy.MockAdapters.RhinoMocks
+{
+    public interface IRhinoMockingFramework : IMockingFramework
+    {
+        void AssertWasCalled<T>(T mock, Action<T> action, Action<IMethodOptions<object>> methodOptions);
+        void AssertWasNotCalled<T>(T mock, Action<T> action, Action<IMethodOptions<object>> methodOptions);
+    }
+}
---
+++ SpecEasy.MockAdapters.RhinoMocks/MockingFramework.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Rhino.Mocks;
+using Rhino.Mocks.Interfaces;
+
+namespace SpecEasy.MockAdapters.RhinoMocks
+{
+    public class MockingFramework : IRhinoMockingFramework
+    {
+        public T GenerateMock<T>(params object[] argumentsForConstructor) where T : class
+        {
+            return MockRepository.GenerateMock<T>();
+        }
+
+        public object GenerateMock(Type type, Type[] extraTypes, params object[] argumentsForConstructor)
+        {
+            return MockRepository.GenerateMock(type, extraTypes, argumentsForConstructor);
+        }
+
+        public void Raise<TEventSource>(TEventSource mockObject, Action<TEventSource> eventSubscription, params object[] args) where TEventSource : class
+        {
+            mockObject.Raise(eventSubscription, args);
+        }
+
+        public void AssertWasCalled<T>(T mock, Action<T> action)
+        {
+            mock.AssertWasCalled(action);
+        }
+
+        public void AssertWasCalled<T>(T mock, Action<T> action, Action<IMethodOptions<object>> methodOptions)
+        {
+            mock.AssertWasCalled(action, methodOptions);
+        }
+
+        public void AssertWasNotCalled<T>(T mock, Action<T> action)
+        {
+            mock.AssertWasNotCalled(action);
+        }
+
+        public void AssertWasNotCalled<T>(T mock, Action<T> action, Action<IMethodOptions<object>> methodOptions)
+        {
+            mock.AssertWasNotCalled(action, methodOptions);
+        }
+    }
+}
