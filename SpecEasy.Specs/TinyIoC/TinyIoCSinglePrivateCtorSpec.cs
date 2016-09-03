7290bd5b425b2b70ea0a2d017c5b92693836d308
Ron Ratzlaff <ron@ratzlaff.us>
2016-09-03 16:46:36 -0600

    Conditional compilation no longer required.
    The test originally referenced a class that was not visible in the release build but it was then changed to use reflection to check the type of exception.

--- SpecEasy.Specs/TinyIoC/TinyIoCSinglePrivateCtorSpec.cs
+++ SpecEasy.Specs/TinyIoC/TinyIoCSinglePrivateCtorSpec.cs
@@ -3,7 +3,6 @@ using Should;
 
 namespace SpecEasy.Specs.TinyIoC
 {
-#if DEBUG
     internal class TinyIoCSinglePrivateCtorSpec : Spec<SinglePrivateCtor>
     {
         public void ConstructorSelection()
@@ -14,6 +13,4 @@ namespace SpecEasy.Specs.TinyIoC
                 Then("an exception should be thrown because we will not use private ctors to build the SUT instance", () => AssertWasThrown<Exception>(ex => ex.GetType().FullName.ShouldStartWith("TinyIoC.TinyIoCResolutionException"))));
         }
     }
-
-#endif
 }
