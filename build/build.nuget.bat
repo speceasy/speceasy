a1e1c427cc2a3459e32884c6ef78e5fe663122fe
James Welle <jwelle@articulate.com>
2018-05-21 14:15:49 -0600

    Obsolete Build Files
     - removed obsolete build files

--- build/build.nuget.bat
+++
@@ -1,7 +0,0 @@
-REM Change path to msbuild if not in system PATH.
-SET MSBUILD="MSBuild.exe"
-
-%MSBUILD% ..\SpecEasy.sln /target:Rebuild /p:Configuration="Release 4.5"
-%MSBUILD% ..\SpecEasy.sln /target:Rebuild /p:Configuration="Release 4.5.1"
-
-..\.nuget\nuget pack ..\SpecEasy\SpecEasy.csproj -Prop Configuration="Release 4.5.1" -Verbosity detailed
